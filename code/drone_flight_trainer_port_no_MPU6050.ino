322504f36b2ad6618e26ee65e750c134b00ef8d4
taha-koroglu <m.tahakoroglu@gmail.com>
2021-03-20 21:28:28 +0300

    Add files via upload

--- code/drone_flight_trainer_port_no_MPU6050.ino
+++ code/drone_flight_trainer_port_no_MPU6050.ino
@@ -1,11 +1,8 @@
-/* Bu kod aşağıdaki kaynaktan alınıp değişikliğe uğramıştır. 
-https://howtomechatronics.com/projects/diy-arduino-rc-transmitter/ */
 #include <eRCaGuy_PPM_Writer.h> // PPM sinyalini oluşturan kütüphane
 const int n = 4; // kanal sayısı, drone projesi için 4 olmalı
 int analogPin[n] = {A0, A1, A2, A3}; // eğer n = 4 ise A2 ve A3'ü de ekleyin
-byte kanal[n];
+uint16_t kanal[n];
 const byte channel[4] = {0, 1, 2, 3}; // Gabriel'ın PPM kütüphanesine erişecek kanalları tanımla
-// The channel us values below are for Radiomaster TX16S radio (remote) controller
 const unsigned int MAX_CHANNEL_VAL = 2100, MIN_CHANNEL_VAL = 900, MIDDLE_CHANNEL_VAL = 1500;
 unsigned int PPMsignal[4] = {MIN_CHANNEL_VAL, MIDDLE_CHANNEL_VAL, MIDDLE_CHANNEL_VAL, MIDDLE_CHANNEL_VAL};
 
@@ -18,9 +15,9 @@ void setup() {
 
 void loop() {
   for (int i=0; i<n; i++)
-      kanal[i] = map(analogRead(analogPin[i]), 0, 1023, 0, 255);
+      kanal[i] = analogRead(analogPin[i]);
   for (int i = 0; i < n; i++)
-      PPMsignal[i] = map(kanal[i], 0, 255, MIN_CHANNEL_VAL, MAX_CHANNEL_VAL);
+      PPMsignal[i] = map(kanal[i], 0, 1023, MIN_CHANNEL_VAL, MAX_CHANNEL_VAL);
   for (int i = 0; i < n; i++)
     PPMWriter.setChannelVal(channel[i], PPMsignal[i]*2);
 }
